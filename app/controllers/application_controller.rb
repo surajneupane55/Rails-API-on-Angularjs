96078d60bf275ff6f96c1880a54f3a6b47dea6a1
Suraj Neupane <surajneupane55@gmail.com>
2016-05-03 21:42:41 +0300

    upgraded version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery
-
-
-  def permission_denied
-    render file: 'public/401.html', layout: false, status: :unauthorized
-  end
+  protect_from_forgery prepend: true
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to authenticated_user_path
-    end
   end
 end
