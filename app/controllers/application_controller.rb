4c4d50c2e1db5def32645c8a014de67ed58a063c
Suraj Neupane <surajneupane55@gmail.com>
2016-05-04 17:33:47 +0300

    final version of api merged in master from branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery prepend: true
+  protect_from_forgery
+
+
+
+  def permission_denied
+    render file: 'public/401.html', layout: false, status: :unauthorized
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,26 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
 
   root 'home#index'
+
+  devise_scope :user do
+    authenticated :user do
+      get '/dashboard' => 'todo#dashboard', as: :authenticated_user
+    end
+  end
+  namespace :api, defaults: {format: :json} do
+    devise_scope :user do
+      resource :session, only:[:create, :destroy]
+    end
+    resources :tasks, only: [:index, :create, :update, :destroy, :show] do
+      resources :lists, only: [:index, :create, :update, :destroy]
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,4 +70,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
